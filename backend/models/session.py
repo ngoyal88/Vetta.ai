5a84d8ca59cd583be2425dcf737455298f7b61d5
Nikhil Goyal <135103317+ngoyal88@users.noreply.github.com>
2025-11-27 00:46:25 +0530

    Removed redundant code

--- backend/models/session.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from typing import Optional
-from datetime import datetime,timezone
-
-class SessionData(BaseModel):
-    user: str
-    role: str
-    interview_type: Optional[str] = None
-    started_at: datetime = datetime.now(timezone.utc)
