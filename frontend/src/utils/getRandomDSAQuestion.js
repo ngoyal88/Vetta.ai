760eceaf4dc10ab67abd8a4417a2270571b8c726
Nikhil Goyal <135103317+ngoyal88@users.noreply.github.com>
2025-11-27 17:49:00 +0530

    delete

--- frontend/src/utils/getRandomDSAQuestion.js
+++
@@ -1,6 +0,0 @@
-import questions from "../data/questions.json";
-
-export const getRandomDSAQuestion = (level = "easy") => {
-  const filtered = questions.filter((q) => q.difficulty === level);
-  return filtered[Math.floor(Math.random() * filtered.length)];
-};
